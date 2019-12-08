04f953252b373b22dc84eae987e7a40b6503f00b
remes2000 <nieruchalskimichal@gmail.com>
2019-12-08 10:35:35 +0100

    Add delete edge and move node features

--- script/main.js
+++ script/main.js
@@ -13,6 +13,7 @@ const height = display.offsetHeight;
 const layer = new Konva.Layer();
 const nodes = [];
 let nodeIdCounter = 0;
+let lineIdCounter = 0;
 let currentLine = null;
 
 const stage = new Konva.Stage({
@@ -40,7 +41,7 @@ document.querySelector('html').addEventListener('keydown', event => {
 
 function drawNode(x, y){
     const nodeGroup = new Konva.Group({
-        id: nodeIdCounter,
+        id: `node${nodeIdCounter}`,
         draggable: true,
         x: 0,
         y: 0
@@ -76,8 +77,23 @@ function drawNode(x, y){
 
     nodeGroup.on('dragmove', e => {
         const virtualNode = nodes.find(n => n.konvaId === nodeGroup._id);
-        const arrow = stage.findOne(virtualNode.arrows[0]+'');
-        arrow.points = getConnectorPoints({x: arrow.points[0], y: arrow.points[1]}, {x: e.currentTarget.getX() + e.currentTarget.children[0].getX(), y: e.currentTarget.getY() + e.currentTarget.children[0].getY()});
+        virtualNode.arrows.forEach(arrowId => {
+            const arrow = layer.findOne('#'+arrowId);
+            const startNode = layer.findOne('#'+arrow.attrs.startNodeId);
+            const endNode = layer.findOne('#'+arrow.attrs.endNodeId);
+            if(arrow.attrs.startNodeId === nodeGroup.attrs.id){
+                arrow.attrs.points = getConnectorPoints(
+                    {x: e.currentTarget.getX() + e.currentTarget.children[0].getX(), y: e.currentTarget.getY() + e.currentTarget.children[0].getY()},
+                    {x: endNode.getX() + endNode.children[0].getX(), y: endNode.getY() + endNode.children[0].getY()},
+                );
+            } else {
+                arrow.attrs.points = getConnectorPoints(
+                    {x: startNode.getX() + startNode.children[0].getX(), y: startNode.getY() + startNode.children[0].getY()},
+                    {x: e.currentTarget.getX() + e.currentTarget.children[0].getX(), y: e.currentTarget.getY() + e.currentTarget.children[0].getY()},
+                );
+            }
+        });
+        layer.draw();
     });
 
     nodeGroup.on('contextmenu', e => {
@@ -95,15 +111,21 @@ function drawNode(x, y){
     });
 
     nodeGroup.add(nodeIdLabel);
-    nodes.push({id: nodeIdCounter, konvaId: nodeGroup._id, arrows: []});
+    nodes.push({id: `node${nodeIdCounter}`, konvaId: nodeGroup._id, arrows: []});
     layer.add(nodeGroup);
     layer.draw();
     nodeIdCounter++;
 }
 
 function deleteNode(node){
+    const virtualNode = nodes.find(n => n.konvaId === node._id);
+    const arrows = [...virtualNode.arrows];
+    arrows.forEach(arrow => {
+        deleteEdge(layer.findOne('#'+arrow));
+    });
+
     node.destroy();
-    nodes.splice(nodes.findIndex(n => n.konvaId === node._id), 1);
+    nodes.splice(nodes.indexOf(virtualNode), 1);
     layer.draw();
 }
 
@@ -112,13 +134,34 @@ function createEdge(node){
         points: [node.getX() + node.children[0].getX(), node.getY() + node.children[0].getY()],
         stroke: 'black',
         strokeWidth: 4,
+        id: `edge${lineIdCounter}`,
+        startNodeId: node.attrs.id
     });
+
+    line.on('contextmenu', e => {        
+        e.evt.preventDefault();
+        const menuItems = [
+            contextMenuItem('Delete edge', () => deleteEdge(e.currentTarget))
+        ];
+        drawContextMenu(e.evt.pageX,e.evt.pageY, menuItems);
+        e.evt.stopPropagation();
+    });
+
+    line.on('mouseover', function() {
+        display.style.cursor = 'pointer';
+    });
+
+    line.on('mouseout', function() {
+        display.style.cursor = 'crosshair';
+    });
+
     currentLine = line;
 
     layer.add(line);
     layer.draw();
     line.moveToBottom();
     layer.draw();
+    lineIdCounter++;
 }
 
 function getConnectorPoints(from, to) {
@@ -154,8 +197,23 @@ function updateLine(x, y){
 function connectLineToNode(node){
     currentLine.attrs.points = getConnectorPoints({x: currentLine.attrs.points[0], y: currentLine.attrs.points[1]}, {x: node.getX() + node.children[0].getX(), y: node.getY() + node.children[0].getY()});
     layer.draw();
+
     const virtualNode = nodes.find(n => n.konvaId === node._id);
-    virtualNode.arrows.push(currentLine._id);
+    virtualNode.arrows.push(currentLine.attrs.id);
+    currentLine.attrs.endNodeId = virtualNode.id;
+
+    const startNode = nodes.find(n => n.id === currentLine.attrs.startNodeId);
+    startNode.arrows.push(currentLine.attrs.id);
 
     currentLine = null;
+}
+
+function deleteEdge(edge){
+    const edgeId = edge.attrs.id;
+    const startNode = nodes.find(n => n.id === edge.attrs.startNodeId);
+    const endNode = nodes.find(n => n.id === edge.attrs.endNodeId);
+    startNode.arrows.splice(startNode.arrows.findIndex(a => a.id === edgeId), 1);
+    endNode.arrows.splice(endNode.arrows.findIndex(a => a.id === edgeId), 1);
+    edge.destroy();
+    layer.draw();
 }
